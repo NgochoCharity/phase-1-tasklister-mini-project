f52348b883d6266daaa781945cb74f84263b83c6
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-06-13 21:12:03 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+document.getElementById('create-task-form').addEventListener('submit', (e) => {
+  e.preventDefault()
+createToDo(document.querySelector('#new-task-description').value);
+
+})
+
+let list = document.createElement('li');
+let btn = document.createElement('button');
+
+function createToDo(toDo){
+  //adds the work to be done on the list tag
+  list.textContent = `${toDo} `;
+  //adds the text done to the button
+  btn.textContent = 'Done';
+  //appends the created list tag to the ToDo list 
+  document.querySelector('#tasks').appendChild(list);
+  //appends the created button to the created
+  list.appendChild(btn);
+  
+};
+btn.addEventListener('click', deletebtn)
+function deletebtn (e){
+e.target.parentNode.remove();
+}
+
+
 });
+
